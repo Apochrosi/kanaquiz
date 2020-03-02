f10467f425f4dcb7bb3bc76a2cc1abcfe2ef2799
Deluge <Deluge@Deluges-MacBook-Air.local>
2020-03-02 02:17:29 +0100

    fix

---
+++ sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.2e3d8f48a28d0dbae5e5fd0246b8da1d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+
